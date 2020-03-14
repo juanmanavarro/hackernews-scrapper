afc65c276a8b86628d3766e5b505746a309e69ec
Juanma <juanaco1.p@gmail.com>
2020-03-14 12:22:00 +0100

    return page 1 if no page requested

--- start/routes.js
+++ start/routes.js
@@ -1,21 +1,23 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const Axios = use('axios')
+const Cherio = use('cheerio')
 
-Route.get('/:page?([0-9]+)', ({ response, params }) => {
+Route.get('/:page([0-9]+)?', async ({ response, params }) => {
+    const page = await Axios.get('https://news.ycombinator.com/')
+    const $ = Cherio.load(page.data);
 
+    const data = $('.athing').map((index, element) => {
+        element = $(element);
+
+        return {
+            id: element.find('.rank').text().slice(0, -1),
+            title: element.find('.storylink').text(),
+            url: element.find('.storylink').attr('href'),
+        }
+    })
+
+    response.json(data.toArray());
 })
--- test/functional/hacker-news.spec.js
+++ test/functional/hacker-news.spec.js
@@ -6,5 +6,15 @@ trait('Test/ApiClient')
 
 test('return 404 if page is not an integer', async ({ client }) => {
   const response = await client.get('/random-string').end()
+
   response.assertStatus(404)
+})
+
+test('return page 1 if no page requested', async ({ assert, client }) => {
+  const response = await client.get('/').end()
+
+  response.assertStatus(200)
+  assert.lengthOf(response.body, 30)
+  assert.ownInclude(response.body[0], { id: '1' })
+  assert.hasAllKeys(response.body[0], [ 'id', 'title', 'url' ])
 })
